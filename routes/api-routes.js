0a89a4ea8d29ad557a6901e5a5c3be6717543159
Ravalash <13k0873juhky@opayq.net>
2020-06-02 21:29:38 -0400

    Adding server settings

---
+++ routes/api-routes.js
@@ -0,0 +1,43 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/trips/:userId", function(req, res) {
+    db.Trip.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  app.get("/api/cities/:tripId", function(req, res) {
+    db.City.findAll({
+      where: {
+        tripId: req.params.tripId
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  app.post("/api/trips", function(req, res) {
+    db.Trip.create(req.body).then(function(result) {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.post("/api/trips", function(req, res) {
+    db.Trip.create({
+      trip_name: req.body.trip_name,
+      start_date: req.body.start_date,
+      end_date: req.body.end_date,
+    })
+      .then(function(result) {
+        res.json(result);
+      });
+  });
+
+
+
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+// *** Dependencies
+// =============================================================
+const express = require("express");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+const db = require("./models");
+
+// Sets up the Express app to handle data parsing
+require('dotenv').config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
