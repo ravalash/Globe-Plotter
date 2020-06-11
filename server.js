d2b1e1b2f0a938278749a41f123e85b8e50517ac
Ravalash <13k0873juhky@opayq.net>
2020-06-10 20:02:05 -0400

    Update API Routes for models

--- server.js
+++ server.js
@@ -10,19 +10,21 @@ const PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 const db = require("./models");
 
-//Require express-sessions 
+//Require express-sessions
 var session = require("express-session");
 
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
 // Sets up the Express app to handle data parsing
-require('dotenv').config();
+require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -40,10 +42,9 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync( { force: true }).then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
