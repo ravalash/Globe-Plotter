4ec3f2ddd7ce641760bf926655f794f32d05c2a8
Joseph Dvorak <dvorakjt@gmail.com>
2020-06-11 00:17:30 -0400

    expandable/collapsible panels added to currenttrip page

--- public/assets/js/currenttrip.js
+++ public/assets/js/currenttrip.js
@@ -113,19 +113,18 @@ $(document).ready(function () {
         let allCitiesDone = true;
         cities.forEach(async (city, index) => {
             //get the activities for this city
-            const newDiv = $('<div class="container" id="sign-card"></div>');
             let activities = await getActivities(city.id);
             activities = activities.data;
             console.log(activities);
             //declare a variable to hold the city name
-            let newHeading;
+            let newHeading = '<div class="container" id="city-card">';
             //if the city is not complete, display the header without strikethrough
             if (city.status == 0) {
-                newHeading = `<h3>${city.city_name}</h3><button class="button-card" data-function="delBtn" data-type="city" data-id="${city.id}">Cancel this city</button>`;
+                newHeading += `<h3>${city.city_name}</h3></div>`;
                 allCitiesDone = false;
             }
             //if the city is complete, display it with strikethrough
-            else newHeading = `<h3><s>${city.city_name}</s></h3><button class="button-card" data-function="delBtn" data-type="city" data-id="${city.id}">Cancel this city</button>`;
+            else newHeading += `<h3><s>${city.city_name}</s></h3></div>`;
             // 
             let newActHtml = '';
             let allDone = true;
@@ -134,33 +133,25 @@ $(document).ready(function () {
                 if (activity.status == 0) { allDone = false; }
 
                 //add a dropdown menu for each activity
-                newActHtml += `<div class="dropdown is-active">
-                <div class="dropdown-trigger">
-                    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                        <span>${activity.status === 0 ? "" : "<s>"}${activity.activity_name}${activity.status === 0 ? "" : "</s>"}</span>
-                        <span class="icon is-small">
-                            <i class="fas fa-angle-down" aria-hidden="true"></i>
-                        </span>
-                    </button>
-                    <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                        <div class="dropdown-content">
-                            <div class="dropdown-item">
-                                <div style="float:left;">
-                                    <img src="${activity.image}" alt="Image of ${activity.activity_name}" id="city-pic" />
-                                </div>
-                                <div style="float:right;">
-                                    <p>${activity.description}</p>
-                                    <br>
-                                    <p>Address: ${activity.address}</p>
-                                    <p>Phone: ${activity.phone}</p>
-                                    <a href="${activity.yelp}">View On Yelp</a>
-                                </div>
-                            </div>
-                        </div>
+                newActHtml += `<div id="progress-card" data-state="hidden" class="activity">
+                <p>${activity.status === 0 ? "" : "<s>"}${activity.activity_name}${activity.status === 0 ? "" : "</s>"}</p>
+                <div hidden>
+                    <div>
+                        <img src="${activity.image}" alt="Image of ${activity.activity_name}" id="city-pic" style="margin-left:1%;"/>
+                    </div>
+                    <div>
+                        <p>${activity.description}</p>
+                        <br>
+                        <p>Address: ${activity.address}</p>
+                        <p>Phone: ${activity.phone}</p>
+                        <a href="${activity.yelp}" style="color:yellow;">View On Yelp</a>
                     </div>
                 </div>
+                <button data-function="checkBtn" data-type="activity" class="button-card" data-id="${activity.id}">Done!</button>
+                <button data-function="delBtn" data-type="activity" class="button-card" data-id="${activity.id}">Cancel</button>
             </div>`
             });
+            newActHtml += '</div>';
             //if all the activities are done, but the city is not complete, add a button to complete it
             if (allDone && city.status == 0) newHeading += `<button class="button-card" data-function="checkBtn" data-type="city" data-id="${city.id}">All done in ${city.city_name}!</button>`;
 
@@ -169,10 +160,9 @@ $(document).ready(function () {
             newActHtml = $(newActHtml);
 
             //append jQuery elements
-            $("#citiesSection").append(newDiv);
-            newDiv.append(newHeading);
-            newDiv.append(newActHtml);
-            if (allCitiesDone) newDiv.append($(`<button class=button-card data-function="tripComplete">This journey is complete!</button>`));
+            $("#citiesSection").append(newHeading);
+            $("#citiesSection").append(newActHtml);
+            if (allCitiesDone) $("#citiesSection").append($(`<button class=button-card data-function="tripComplete">This journey is complete!</button>`));
         });
     }
 
@@ -211,6 +201,19 @@ $(document).ready(function () {
         else if (button.attr("data-function") === "tripComplete") {
             await updateTripStatus(trip, 2);
             window.location.replace("/dashboard");
+        }
+
+        else if (button.attr("class") === "activity") {
+            let state = button.attr("data-state");
+            if (state === "hidden") {
+                button.attr("data-state", "displayed");
+                button.children("div").removeAttr("hidden");
+            }
+            else {
+                console.log("yo");
+                button.attr("data-state", "hidden");
+                button.children("div").attr("hidden", "");
+            }
         }
     });
 });
