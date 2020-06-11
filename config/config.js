c307e91276f0777af156f6af776fbd25fddc5903
Ravalash <13k0873juhky@opayq.net>
2020-06-11 16:00:15 -0400

    Updating Heroku

--- config/config.js
+++ config/config.js
@@ -17,11 +17,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_BASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    JAWSDB_URL: "mysql://nx11bchkqv5hujzs:f5uhgc6dc5hyau1x@ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zpdkmn5jiayztj9b",
     dialect: "mysql",
   },
 };
